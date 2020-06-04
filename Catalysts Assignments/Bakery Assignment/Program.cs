9f8361bfb11cda0bdeea2129ee8520220f55cb6f
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-06-05 00:46:27 +0200

    Beginning

---
+++ Catalysts Assignments/Bakery Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bakery_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
