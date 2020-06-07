5de495bc723f6c91b25de8bd1a890ce7a662824b
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-06-07 21:28:03 +0200

    something

--- Catalysts Assignments/Bakery Assignment/Program.cs
+++ Catalysts Assignments/Bakery Assignment/Program.cs
@@ -6,7 +6,37 @@ namespace Bakery_Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program myprogram = new Program();
+            myprogram.Start();
+        }
+        void Start()
+        {
+            DaysDailySales();
+            Console.ReadKey();
+        }
+        public int DaysDailySales()
+        {
+            int day = 1;
+            int count = 0;
+
+            while (day != 0)
+            {
+                Console.Write("Enter the day: ");
+                day = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the daily sales: ");
+                int dailySales = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the payment in the bank: ");
+                int paymentBank = int.Parse(Console.ReadLine());
+
+                if (dailySales > paymentBank)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Days where daily sales were more than payment in bank: " + count);
+            return count;
         }
     }
 }
