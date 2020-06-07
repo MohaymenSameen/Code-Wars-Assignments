b8ff3bee9056feb5d54f933c323d266e23cef701
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-06-08 01:31:15 +0200

    Jaden assignment uppercase

---
+++ Catalysts Assignments/JadenUpperCase/Program.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace JadenUpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        public string ToJadenCase(this string phrase)
+        {
+            string[] sentence = phrase.Split(' ');
+            string newPhrase = "";
+            foreach (String word in sentence)
+            {
+                char firstLetter = Char.ToUpper(word[0]);
+                newPhrase += firstLetter + word.Substring(1) + " ";
+            }
+            return newPhrase;
+        }
+        public void FixedTest()
+        {
+            Assert.AreEqual("How Can Mirrors Be Real If Our Eyes Aren't Real ",
+                            "How can mirrors be real if our eyes aren't real".ToJadenCase(),
+                            "Strings didn't match.");
+        }
+    }
+}
