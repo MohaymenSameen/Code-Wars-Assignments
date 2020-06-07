288a2add70d45d6bd37b4b5544f20e653751703f
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-06-07 22:27:00 +0200

    tried to do it

--- Catalysts Assignments/Bakery Assignment/Program.cs
+++ Catalysts Assignments/Bakery Assignment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Bakery_Assignment
 {
@@ -11,19 +13,25 @@ namespace Bakery_Assignment
         }
         void Start()
         {
-            DaysDailySales();
+            Dictionary<int,int> sales = DailySales("daily.txt");
+            Dictionary<int,int> amount = Deposited("deposited.txt");
+
             Console.ReadKey();
         }
-        public int DaysDailySales()
+        public List<int> DaysDailySales(Dictionary<int,int> sales, Dictionary<int,int>amount)
         {
             int day = 1;
-            int count = 0;
+            List<int> days = new List<int>();
 
             while (day != 0)
             {
                 Console.Write("Enter the day: ");
                 day = int.Parse(Console.ReadLine());
 
+                if(day == 0)
+                {
+                    break;
+                }
                 Console.Write("Enter the daily sales: ");
                 int dailySales = int.Parse(Console.ReadLine());
 
@@ -32,11 +40,55 @@ namespace Bakery_Assignment
 
                 if (dailySales > paymentBank)
                 {
-                    count++;
+                    days.Add(day);
                 }
-            }
-            Console.WriteLine("Days where daily sales were more than payment in bank: " + count);
-            return count;
+            }            
+            return days;
         }
+        void DisplayDays(List<int> days)
+        {
+            int count = 1;
+            foreach (int day in days)
+            {
+                Console.WriteLine("{0} => {1} ",count,day);
+                count++;
+            }
+        }
+        public Dictionary<int,int> DailySales(string filename)
+        {
+            Dictionary<int, int> dailySales = new Dictionary<int, int>();
+            StreamReader reader = new StreamReader(filename);
+            string line = "";
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                string[] words = line.Split(' ');
+
+                int day = Convert.ToInt32(words[1]);
+                int sales = Convert.ToInt32(words[2]);
+
+                dailySales.Add(day, sales);               
+            }
+            reader.Close();
+            return dailySales;
+        }
+        public Dictionary<int, int> Deposited(string filename)
+        {
+            Dictionary<int, int> dailySales = new Dictionary<int, int>();
+            StreamReader reader = new StreamReader(filename);
+            string line = "";
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                string[] words = line.Split(' ');
+
+                int day = Convert.ToInt32(words[1]);
+                int sales = Convert.ToInt32(words[2]);
+
+                dailySales.Add(day, sales);
+            }
+            reader.Close();
+            return dailySales;
+        }        
     }
 }
