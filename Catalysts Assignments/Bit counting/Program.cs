47abc080f6957a1cbbf8c79f3b1de69be25a8ded
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-06-09 03:32:32 +0200

    binary numbers

---
+++ Catalysts Assignments/Bit counting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bit_counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprogram = new Program();
+            myprogram.Start();
+        }
+        void Start()
+        {
+            CountBits(1234);
+            Console.ReadKey();
+        }
+        public static int CountBits(int n)
+        {
+            string binary = Convert.ToString(n, 2);            
+            int count = 0;
+
+            foreach (Char num in binary)
+            {
+                count += num-48;
+                Console.WriteLine(count);
+            }
+            
+
+            return count;
+        }
+    }
+}
