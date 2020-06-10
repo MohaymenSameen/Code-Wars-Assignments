e344fa2cc3a94f9cdd3ebd6f773ea726bb2af9f9
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-06-10 02:11:04 +0200

    new assignments

---
+++ Catalysts Assignments/Dont Give Me Five!/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dont_Give_Me_Five_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprogram = new Program();
+            myprogram.Start();
+        }
+        void Start()
+        {
+            DontGiveMeFive(4, 17);
+            Console.ReadKey();
+        }
+        public static int DontGiveMeFive(int start, int end)
+        {
+            int count = start;
+
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine(count);
+                count++;
+
+                string numbers = count.ToString();
+
+                
+                Console.WriteLine(numbers);
+            }
+            return count;
+        }
+    }
+}
---
+++ Catalysts Assignments/Persistent bugger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Persistent_bugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprogram = new Program();
+            myprogram.Start();
+        }
+        void Start()
+        {
+            Persistence(39);
+            Console.ReadKey();
+        }
+        public static int Persistence(long n)
+        {       
+            //still needs to be completed
+            string num = Convert.ToString(n);
+            
+            if (num.Length == 1)
+            {
+                return 0;
+            }
+            int count = 0;
+            int multiply = 1;
+            for (int i = 0; i < num.Length; i++)
+            {
+                multiply *= num[i];
+                //Console.WriteLine(multiply);
+            }
+            var result = num.ToArray().Select(x => int.Parse(x.ToString())).Aggregate((a, b) => a * b);
+            Console.WriteLine(result);
+            return multiply;
+        }
+    }
+}
