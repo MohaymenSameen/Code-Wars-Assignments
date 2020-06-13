279be4e206030d27172ed5b244d068422d7e8ea6
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-06-13 23:29:39 +0200

    fix

--- Catalysts Assignments/Dont Give Me Five!/Program.cs
+++ Catalysts Assignments/Dont Give Me Five!/Program.cs
@@ -13,11 +13,20 @@ namespace Dont_Give_Me_Five_
         }
         void Start()
         {
-            DontGiveMeFive(4, 17);
+            DontGiveMeFive(1, 9);
             Console.ReadKey();
         }
         public static int DontGiveMeFive(int start, int end)
         {
+            int count = 0;
+            for (int i = start; i < end; i++)
+            {
+                if (!i.ToString().Contains("5"))
+                {
+                    count++;
+                }
+                Console.WriteLine(count);
+            }
             return Enumerable.Range(start, end - start + 1).Count(x => !x.ToString().Contains("5"));
         }
     }
