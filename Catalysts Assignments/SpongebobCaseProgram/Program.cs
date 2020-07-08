73c55961debfe73260003a583073d853c6d53228
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-07-08 14:07:45 +0200

    spongebob case

---
+++ Catalysts Assignments/SpongebobCaseProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SpongebobCaseProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            Console.Write("Enter your sentence: ");
+            string sentence = Console.ReadLine();
+            inputSentence(sentence);
+            Console.ReadKey();
+        }
+        public string inputSentence(string sentence)
+        {
+            int count = 0;
+            string newSentence = "";
+            foreach (char c in sentence)
+            {
+                if (count%2==0)
+                {
+                    newSentence += Char.ToUpper(c);                    
+                }
+                else
+                {
+                    newSentence += Char.ToLower(c);
+                }
+                count++;
+            }
+            Console.WriteLine(newSentence);
+
+            return sentence;
+        }        
+    }
+}
